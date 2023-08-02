a8b8809422c7a5a2909ce0001d75745600fdf9b1
Paula Garcia <paula.garcia.9899@gmail.com>
2023-08-02 13:57:37 -0400

    Hasta task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,58 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {
+        "username": "Paula",
+        "password": "paulapassword"
+    }
+];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+  const isValid = (username)=>{
+    let userwithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userwithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    let validusers= users.filter((user) =>{
+        return (user.username === username && user.password === password);
+    });
+
+    if(validusers.length > 0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password){
+      return res.status(404).json({message: "Please complete all fields (username,pass)"});
+  }
+
+  if(authenticatedUser(username,password)){
+      let accessToken= jwt.sign({
+          data: password
+      }, 'access', {expiresIn: 60*60});
+
+      req.session.authorization = {
+          accessToken,username
+      }
+      return res.status(200).send("User Logged in!!");
+  } else{
+      return res.status(208).json({message:"Invalid Login: check pass and username"});
+  }
+
 });
 
 // Add a book review
